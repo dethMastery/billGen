ebe900500372a04a64643c2be3eea70d2fe672b6
dethMastery <me@suphakit.net>
2023-06-08 21:07:11 +0700

    chore: template

--- src/static/_template.js
+++ src/static/_template.js
@@ -1,7 +1,10 @@
 const navBar = require('./components/navBar.js')
+const footer = require('./components/footer.js')
 
 function template(body) {
   return html = `${navBar}
+${body}
+${footer}
 
 <script src="/js/theme.js"></script>`
 }
