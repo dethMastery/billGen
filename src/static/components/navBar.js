d4d65abb1fa7956fc9f8fd1ede12862382b8e873
dethMastery <me@suphakit.net>
2023-06-08 20:54:47 +0700

    feat: navBar Components

---
+++ src/static/components/navBar.js
@@ -0,0 +1,12 @@
+const navBar = `<div id="navBar">
+  <div class="left">
+    <a href="/">
+      katzuBilling
+    </a>
+  </div>
+  <div class="right">
+    <button onclick="mode()" id="themeButton"></button>
+  </div>
+</div>`
+
+module.exports = navBar
