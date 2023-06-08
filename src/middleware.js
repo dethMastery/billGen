fac25106a60526e80ded1d74718f3953399e670e
dethMastery <me@suphakit.net>
2023-06-09 04:18:31 +0700

    chore: fix middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,13 @@
 const path = require('path')
 const bodyParser = require('body-parser')
 
-const { exp, app, root } = require("../app");
+const { exp, app, root } = require('../app')
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 app.use(exp.static(path.join(root, 'static')))
-app.use(bodyParser.json())
