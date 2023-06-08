ea83199b1ad5ba6a40befb20168c42c62d738b83
dethMastery <me@suphakit.net>
2023-06-08 20:40:43 +0700

    chore: index page

---
+++ src/static/index.js
@@ -0,0 +1,11 @@
+const template = require('../static/_template.js')
+
+const title = 'katzuBill'
+const head = ``
+const body = ``
+
+module.exports = {
+  title: title,
+  head: head,
+  body: template(body)
+}
