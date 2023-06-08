e391ee377e42fecb4a8001d353271da84f61c1cb
dethMastery <me@suphakit.net>
2023-06-08 20:42:05 +0700

    chore: pageLayout

--- src/static/__Layout.js
+++ src/static/__Layout.js
@@ -4,6 +4,9 @@ function pageLayout(data) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>${data.title}</title>
+  
+  <link rel="stylesheet" href="/css/main.css">
+  ${data.head}
 </head>`
 
   const body = `<body>
