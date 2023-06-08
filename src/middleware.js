5b3a1ebc3cc970d33f5c7538eb65a8c27b5dfa2a
dethMastery <me@suphakit.net>
2023-06-09 04:08:18 +0700

    feat: bodyparser

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const bodyParser = require('body-parser')
+
 const { exp, app, root } = require("../app");
 
 app.use(exp.static(path.join(root, 'static')))
+app.use(bodyParser.json())
