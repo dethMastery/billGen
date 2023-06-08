09fb57b3d14112d543a8046d16a570abfaba0ae9
dethMastery <me@suphakit.net>
2023-06-08 20:08:09 +0700

    chore: layout

---
+++ src/static/__Layout.js
@@ -0,0 +1,20 @@
+function pageLayout(data) {
+  const head = `<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>${data.title}</title>
+</head>`
+
+  const body = `<body>
+  ${data.body}
+</body>`
+
+  return html = `<!DOCTYPE html>
+<html lang="en">
+  ${head}
+  ${body}
+</html>`
+}
+
+module.exports = pageLayout
