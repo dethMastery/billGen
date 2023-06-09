cbae54990a053a1249c08965f601761280e91c17
dethMastery <me@suphakit.net>
2023-06-09 23:46:01 +0700

    chore: fix QRCode config

--- src/api/qr.js
+++ src/api/qr.js
@@ -1,25 +1,31 @@
 const genPayload = require('promptpay-qr')
+const qrcode = require('qrcode')
 
-function qrGenerator(data) {
+function qrGenerator(res, data) {
   const amount = parseInt(data.amount)
-  const payLoad = genPayload(data.order, {amount})
+  const payLoad = genPayload(data.order, { amount })
 
   async function genQR() {
     const options = {
       type: 'svg',
       color: {
         dark: '#f5f5f5',
-        light: '#fe6e6f'
-      }
+        light: '#fe6e6f',
+      },
     }
 
-    await new PromiseRejectionEvent((resolve, reject) => {
+    await new Promise((resolve, reject) => {
       qrcode.toString(payLoad, options, (err, svg) => {
         if (err) return reject(err)
 
         resolve(svg)
-        return svg
+        res.set('Content-Type', 'image/svg+xml')
+        res.send(svg)
       })
     })
   }
+
+  genQR()
 }
+
+module.exports = qrGenerator
